0b1a80991b84848c94e6577870f90a03c034532b
Yash Ramoliya <yashramoliya12@gmail.com>
2023-08-12 01:18:29 +0530

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended",
+    "airbnb-typescript/base",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts"],
+    },
+  },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+  ignorePatterns: ["**/*.js", "node_modules", "dist", "coverage"],
+};
